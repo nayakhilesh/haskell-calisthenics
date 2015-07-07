490f690e774e736f9aac4a928d9034468b4ab016
Akhilesh Nayak <nayakhilesh@gmail.com>
2015-07-07 19:32:13 -0400

    finished HW06

--- HW06.hs
+++ HW06.hs
@@ -34,51 +34,69 @@ streamToList (Cons first rest) = first : streamToList rest
 -- Exercise 4 -----------------------------------------
 
 instance Functor Stream where
-    fmap = undefined
+    fmap f (Cons first rest) = Cons (f first) (fmap f rest)
 
 -- Exercise 5 -----------------------------------------
 
 sRepeat :: a -> Stream a
-sRepeat = undefined
+sRepeat a = Cons a (sRepeat a)
 
 sIterate :: (a -> a) -> a -> Stream a
-sIterate = undefined
+sIterate f seed = Cons seed (sIterate f (f seed))
 
 sInterleave :: Stream a -> Stream a -> Stream a
-sInterleave (Cons _ _) _ = undefined
+sInterleave (Cons first rest) secondStream = Cons first (sInterleave secondStream rest)
 
 sTake :: Int -> Stream a -> [a]
-sTake = undefined
+sTake 0 _ = []
+sTake n (Cons first rest) = first : sTake (n - 1) rest
 
 -- Exercise 6 -----------------------------------------
 
 nats :: Stream Integer
-nats = undefined
+nats = sIterate (+1) 0
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = foldr1 sInterleave (map sRepeat [0..])
 
 -- Exercise 7 -----------------------------------------
 
 -- | Implementation of C rand
 rand :: Int -> Stream Int
-rand = undefined
+rand seed = Cons random (rand random)
+			where random = (1103515245 * seed + 12345) `mod` 2147483648
 
 -- Exercise 8 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: 221 MB -}
 minMaxSlow :: [Int] -> Maybe (Int, Int)
 minMaxSlow [] = Nothing   -- no min or max if there are no elements
 minMaxSlow xs = Just (minimum xs, maximum xs)
 
 -- Exercise 9 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: 213 MB -}
 minMax :: [Int] -> Maybe (Int, Int)
-minMax = undefined
+minMax [] = Nothing
+minMax (first:rest) = 
+	case (minMax rest) of 
+					Nothing -> Just (first, first)
+					Just (restMin, restMax) -> Just (min first restMin, max first restMax)
+
+{- Total Memory in use: 203 MB -}
+minMaxFast :: [Int] -> Maybe (Int, Int)
+minMaxFast [] = Nothing
+minMaxFast xs = minMaxFastHelper minBound maxBound xs
+
+minMaxFastHelper :: Int -> Int -> [Int] -> Maybe (Int, Int)
+minMaxFastHelper prevMin prevMax [] = Just (prevMin, prevMax)
+minMaxFastHelper prevMin prevMax (first:rest) = minMaxFastHelper newMin newMax rest
+										  		 where 
+										  		 	newMin = min prevMin first
+										  			newMax = max prevMax first
 
 main :: IO ()
-main = print $ minMaxSlow $ sTake 1000000 $ rand 7666532
+main = print $ minMaxFast $ sTake 1000000 $ rand 7666532
 
 -- Exercise 10 ----------------------------------------
 
